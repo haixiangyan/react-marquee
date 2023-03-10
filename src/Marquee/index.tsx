7858f0f56a0be5f4583c5e2bfbec1b4d1e2d5d5f
haixiangyan <haixiang6123@gmail.com>
2023-03-10 23:38:28 +0800

    feat: 去掉速度

--- src/Marquee/index.tsx
+++ src/Marquee/index.tsx
@@ -3,49 +3,18 @@ import styles from './styles.module.scss';
 import classNames from "classnames";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-  speed?: number;
 }
 
 export const Marquee: FC<Props> = (props) => {
-  const { speed = 20, children, className, ...restProps } = props;
-
-  const [containerWidth, setContainerWidth] = useState<number>(0);
-  const [marqueeWidth, setMarqueeWidth] = useState<number>(0);
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const marqueeRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const getWidth = () => {
-      if (containerRef.current && marqueeRef.current) {
-        const containerRect = containerRef.current.getBoundingClientRect();
-        const marqueeRect = marqueeRef.current.getBoundingClientRect();
-
-        setContainerWidth(containerRect.width);
-        setMarqueeWidth(marqueeRect.width);
-      }
-    }
-
-    getWidth();
-
-    window.addEventListener('resize', getWidth)
-
-    return () => {
-      window.removeEventListener('resize', getWidth)
-    }
-  }, []);
-
-  const duration = useMemo(() => {
-    return marqueeWidth < containerWidth ? containerWidth / speed : marqueeWidth / speed;
-  }, [containerWidth, marqueeWidth]);
+  const { children, className, ...restProps } = props;
 
   return (
-    <div ref={containerRef} className={classNames(styles.marquee, className)} {...restProps}>
-      <div ref={marqueeRef} className={styles.content} style={{ animationDuration: `${duration}s` }}>
+    <div className={classNames(styles.marquee, className)} {...restProps}>
+      <div className={styles.content}>
         {children}
       </div>
 
-      <div className={styles.content} style={{ animationDuration: `${duration}s` }}>
+      <div className={styles.content}>
         {children}
       </div>
     </div>
