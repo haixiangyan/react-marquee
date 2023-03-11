ea742da758ae1c6970d577bfdd9e42eccd1ef973
haixiangyan <haixiang6123@gmail.com>
2023-03-11 10:27:20 +0800

    feat: 使用 speed 来替换 duration，解决速度不一致的问题

--- src/Marquee/index.tsx
+++ src/Marquee/index.tsx
@@ -1,9 +1,9 @@
-import {CSSProperties, FC, HTMLAttributes} from "react";
+import {CSSProperties, FC, HTMLAttributes, useEffect, useMemo, useRef, useState} from "react";
 import styles from './styles.module.scss';
 import classNames from "classnames";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-  duration?: CSSProperties['animationDuration'];
+  speed?: number;
   gradientColor?: string;
   gradientWidth?: CSSProperties['width'];
   pauseOnHover?: boolean;
@@ -11,7 +11,20 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const Marquee: FC<Props> = (props) => {
-  const { direction = 'ltr', duration = '10s', pauseOnHover, gradientColor, gradientWidth = 200, children, className, ...restProps } = props;
+  const { direction = 'ltr', speed = 10, pauseOnHover, gradientColor, gradientWidth = 200, children, className, ...restProps } = props;
+
+  const [containerWidth, setContainerWidth] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // 获取容器宽度
+    if (containerRef.current) {
+      setContainerWidth(containerRef.current.getBoundingClientRect().width);
+    }
+  }, []);
+
+  // 计算动画时长
+  const duration = useMemo(() => containerWidth / speed, [containerWidth, speed]);
 
   return (
     <div
@@ -21,11 +34,11 @@ export const Marquee: FC<Props> = (props) => {
       })}
       {...restProps}
     >
-      <div className={styles.content} style={{ animationDuration: duration }} >
+      <div ref={containerRef} className={styles.content} style={{ animationDuration: `${duration}s` }} >
         {children}
       </div>
 
-      <div className={styles.content} style={{ animationDuration: duration }} >
+      <div className={styles.content} style={{ animationDuration: `${duration}s` }} >
         {children}
       </div>
 
